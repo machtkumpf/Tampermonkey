295e29a03c98398aa40defd910273fb452683cf8
machtkumpf <wood.kyle.s@rutgers.edu>
2015-05-19 12:14:30 -0400

    Update 1.1

--- FreshService Refresh.user.js
+++ FreshService Refresh.user.js
@@ -4,10 +4,12 @@
 // @description This is a simple script, it will refresh the page every x seconds
 // @include http://ibiquity.freshservice.com/helpdesk/dashboard
 // @include https://ibiquity.freshservice.com/helpdesk/dashboard
+// @include http://helpdesk.ibiquity.com/helpdesk
+// @include https://helpdesk.ibiquity.com/helpdesk
 // @features Super sweet refreshing
-// @CodedBy AyoobAli
+// @CodedBy Wood
 // @Website http://www.AyoobAli.com
-// @version 1.0.0
+// @version 1.2
 // ==/UserScript==
 
 //===[Settings]===\\
--- Freshservice Center.user.js
+++ Freshservice Center.user.js
@@ -1,11 +1,14 @@
 // ==UserScript==
 // @name         Freshservice Center
 // @namespace    http://your.homepage/
-// @version      0.1
+// @version      1.1
 // @description  enter something useful
 // @author       You
 // @include        http://ibiquity.freshservice.com/helpdesk/dashboard
 // @include http://ibiquity.freshservice.com/helpdesk/dashboard
+// @include https://ibiquity.freshservice.com/helpdesk/dashboard
+// @include http://helpdesk.ibiquity.com/helpdesk
+// @include https://helpdesk.ibiquity.com/helpdesk
 // @grant        none
 // ==/UserScript==
 
